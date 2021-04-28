206ca1fcf3843c79a9e1b3123bf1458ea561b850
Michael Valdes <michael.j.valdes@gmail.com>
2021-04-27 21:19:57 -0400

    Add angular flexbox and demo module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'demo', loadChildren: () => import('./demo/demo.module').then(m => m.DemoModule) },
+  { path: '**', redirectTo: 'demo'}
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
     BrowserModule,
     BrowserAnimationsModule,
+    MaterialModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
